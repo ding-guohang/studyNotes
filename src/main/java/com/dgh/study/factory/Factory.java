6815faad48bbf5a5d48e7c4f0ad314614004e3cf
guohang.ding <guohang.ding@qunar.com>
2016-08-31 09:34:52 +0800

    init factory description

--- src/main/java/com/dgh/study/factory/Factory.java
+++ src/main/java/com/dgh/study/factory/Factory.java
@@ -1,6 +1,13 @@
 package com.dgh.study.factory;
 
 /**
+ * Description:
+ * Define an interface for creating an object, but let subclasses decide which class
+ * to instantiate. Factory method lets a class defer instantiation to subclasses.
+ *
+ * 定义：
+ * 定义一个创建对象的接口，但由子类决定实例化哪一个类。
+ * 工厂模式让类的实例化延迟到其子类
  * @author guohang.ding on 16-8-29
  */
 public class Factory {
