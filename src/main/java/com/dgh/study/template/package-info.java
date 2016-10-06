58a301a323907a48e77ae4e2e4bc4490382dbdb8
ding-guohang <1483541826@qq.com>
2016-10-06 08:54:29 +0800

    finish template

---
+++ src/main/java/com/dgh/study/template/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * 模板方法模式
+ *
+ * Define the skeleton of an algorithm in an operation, deferring some steps to subclass.
+ * Template Method lets subclasses redefine certain steps of an algorithm without changing the algorithm's structure
+ * 定义一个操作中的算法框架，将一些步骤延迟到子类。
+ * 模板方法 使得子类可以在不改变算法的步骤顺序的情况下，重定义该算法的某些特定步骤
+ *
+ * 感觉挺好的，像Pay里面应该用得上，比如MultiRefund and HeDanMultiRefund
+ *
+ * @author guohang.ding on 2016/10/6.
+ */
+package com.dgh.study.template;
