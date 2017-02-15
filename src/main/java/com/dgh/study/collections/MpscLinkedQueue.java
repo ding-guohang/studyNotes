c54273f345f39323c5e3eb0bc67d33696ace3f58
guohang.ding <guohang.ding@qunar.com>
2017-02-15 19:57:22 +0800

    add log, add csdn tomorrow

--- src/main/java/com/dgh/study/collections/MpscLinkedQueue.java
+++ src/main/java/com/dgh/study/collections/MpscLinkedQueue.java
@@ -2,9 +2,19 @@ package com.dgh.study.collections;
 
 /**
  * MpscLinkedQueue
- * 从QMQ里面看到了这个队列, 发现netty有一个类似的实现
+ * 从QMQ里面看到了这个队列, 发现是复制并少量修改了netty的实现
  * 大致是无锁队列...适用于单消费者多生产者场景
- * 依赖CAS完成入队操作
+ * <p>
+ * <p>
+ * 其实不支持remove就很容易理解了.
+ * 无锁队列之所以可以无锁并支持多生产者模型, 就是用一种微弱的稳定性串联起了offer的并发操作
+ * 一旦允许remove, remove头尾相关节点都有可能导致链表断裂
+ *
+ * 里面有三种需要提一下的数据结构
+ * Node, DefaultNode, Ref
+ * 1.Node  声明了的next, volatile型, 还有AtomicReferenceFieldUpdater对next进行修改
+ * 2.DefaultNode  Node的实现类, 声明了value
+ * 3.Ref  分为TailRef和HeadRef, 分别声明了volatile的tailRef和headRef引用...还有各自的AtomicReferenceFieldUpdater
  *
  * @author guohang.ding on 17-2-13
  */
