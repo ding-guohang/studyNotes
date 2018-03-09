64fd9d6663a91c6ff8f3a75e4942bc8eaf4ad643
guohang.ding <guohang.ding@qunar.com>
2018-03-09 13:09:44 +0800

    finish rob and add some questions

--- src/main/java/com/dgh/work/dp/Rob.java
+++ src/main/java/com/dgh/work/dp/Rob.java
@@ -10,13 +10,34 @@ package com.dgh.work.dp;
 public class Rob {
 
     /**
-     * dp(i, j) 表示
-     *
+     * f(n) = 包含索引为n的所有房子的最大金额
+     * g(n) = 索引为n的房子的金额
+     * <p>
+     * f(n) = max{ f(n-2)+g(n), f(n-1) }
      */
     public int rob(int[] nums) {
+        if (0 == nums.length) { //f(0)
+            return 0;
+        }
 
+        if (1 == nums.length) { //f(1)
+            return nums[0];
+        }
 
+        int a = 0;  // f(n-2), default is f(0)
+        int b = nums[0]; // f(n-1), default is f(1)
+        int now = 0;
 
-        return 0;
+        // begins with n = 2
+        for (int i = 1; i < nums.length; i++) {
+            now = max(a + nums[i], b);
+            a = b;
+            b = now;
+        }
+        return now;
+    }
+
+    private int max(int a, int b) {
+        return a > b ? a : b;
     }
 }
--- src/main/java/com/dgh/work/package-info.java
+++ src/main/java/com/dgh/work/package-info.java
@@ -1,5 +1,9 @@
 /**
  * For Work
+ * <p>
+ * Questions:
+ * 1. 一个数组，有正有负，求最大连续元素相加的最大值
+ * 2.二叉树求两个节点最大距离
  *
  * @author guohang.ding on 2018/3/2.
  */
